eca45c519e6be36a69bf3db0251db37b834c8d71
Shin <sekolby20@gmail.com>
2020-02-24 16:04:46 -0500

    sgsrdfsrgdf

--- js/text.js
+++ js/text.js
@@ -1,12 +1,13 @@
 
 
+
+window.onload = function() {
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 var loader = new THREE.GLTFLoader();
-
 camera.position.z = 5;
 
 loader.load( 'js/sean.glb', function ( gltf ) {
@@ -14,3 +15,4 @@ loader.load( 'js/sean.glb', function ( gltf ) {
 }, undefined, function ( error ) {
 	console.error( error );
 } );
+}
