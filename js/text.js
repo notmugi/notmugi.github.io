e44379010f5ad36e1e4d32559d33385b846f008d
Shin <sekolby20@gmail.com>
2020-02-24 15:00:09 -0500

    aa

--- js/text.js
+++ js/text.js
@@ -1,30 +1,24 @@
-var textmesh;
-var maxX, maxY, maxZ;
-var first=true;
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-window.onload = function()
-{
-    init();
-    //animate();
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+var animate = function () {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
 };
 
-function init()
-{
-//init
-		var scene = new THREE.Scene();
-    var sceneWidth = window.innerWidth/2;
-    var sceneHeight = window.innerHeight/2 + 100;
-		var loader = new THREE.GLTFLoader();
-		var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-		var renderer = new THREE.WebGLRenderer();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		document.body.appendChild( renderer.domElement );
-
-//loader
-		loader.load( 'sean.gltf', function ( gltf ) {
-			scene.add( gltf.scene );
-			}, undefined, function ( error ) {
-				console.error( error );
-			} );
-}
-//
+animate();
