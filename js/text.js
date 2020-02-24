00d5407879314bae23ab2bfc9234058fdedb45d7
Shin <sekolby20@gmail.com>
2020-02-24 15:19:52 -0500

    aaaaadwad

--- js/text.js
+++ js/text.js
@@ -1,7 +1,6 @@
 var camera;
 var scene;
 var renderer;
-var mesh1, mesh2, mesh3, mesh4, mesh5, mesh6, mesh7, mesh8, mesh9;
 var maxX, maxY, maxZ;
 var first=true;
 
@@ -13,6 +12,7 @@ window.onload = function()
 
 function init()
 {
+		var loader = new THREE.GLTFLoader();
 		var sceneWidth = window.innerWidth/2;
 		var sceneHeight = window.innerHeight/2 + 100;
 		// camera: vertical-field-of-view, aspect-ratio, near, far
@@ -20,42 +20,21 @@ function init()
 		camera.position.z = 1000;
 
 		// Define the geometry of the object
-		geometry1 = new THREE.OctahedronGeometry( 200, 0 );
-		geometry2 = new THREE.IcosahedronGeometry( 200, 0 );
-		geometry3 = new THREE.PlaneGeometry( 200, 200, 10, 10 );
-		geometry4 = new THREE.CylinderGeometry( 200, 300, 300, 32, 3, true );
-		geometry5 = new THREE.TetrahedronGeometry( 200, 0 );
-		geometry6 = new THREE.CubeGeometry( 200, 200, 200, 3, 3, 3 );
-		geometry7 = new THREE.TorusGeometry( 200, 32, 8, 32 );
-		geometry8 = new THREE.TorusKnotGeometry( 200, 32, 8, 16 , 1, 2);
-		geometry9 = new THREE.SphereGeometry( 200, 32, 32 );
 
 		// An object becomes visible only if its material-type is defined.
 		material1 = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 		material2 = new THREE.MeshNormalMaterial( { color: 0xff0000, wireframe: false } );
 
 		// Mesh actually creates the 3D object from the geometry and the material
-		mesh1 = new THREE.Mesh( geometry1, material1 );
-		mesh2 = new THREE.Mesh( geometry2, material1 );
-		mesh3 = new THREE.Mesh( geometry3, material1 );
-		mesh4 = new THREE.Mesh( geometry4, material1 );
-		mesh5 = new THREE.Mesh( geometry5, material2 );
-		mesh6 = new THREE.Mesh( geometry6, material2 );
-		mesh7 = new THREE.Mesh( geometry7, material1 );
-		mesh8 = new THREE.Mesh( geometry8, material1 );
-		mesh9 = new THREE.Mesh( geometry9, material1 );
 
 		scene = new THREE.Fog(0x55AAFF);
 		scene = new THREE.Scene();
-		scene.add( mesh1 );
-		scene.add( mesh2 );
-		scene.add( mesh3 );
-		scene.add( mesh4 );
-		scene.add( mesh5 );
-		scene.add( mesh6 );
-		scene.add( mesh7 );
-		scene.add( mesh8 );
-		scene.add( mesh9 );
+
+		loader.load( 'path/to/model.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
 
 		renderer = new THREE.WebGLRenderer();
 		renderer.setSize( sceneWidth, sceneHeight );
@@ -68,32 +47,9 @@ function init()
 		/* Move the objects away from each other */
 		var x1=560, x2=-560;
 
-		mesh1.position.x = x2;
-		mesh1.position.y = x1;
+		//mesh1.position.x = x2;
+		//mesh1.position.y = x1;
 
-		mesh2.position.x = 0;
-		mesh2.position.y = x1;
-
-		mesh3.position.x = x1;
-		mesh3.position.y = x1;
-
-		mesh4.position.x = x2;
-		mesh4.position.y = 0;
-
-		mesh5.position.x = 0;
-		mesh5.position.y = 0;
-
-		mesh6.position.x = x1;
-		mesh6.position.y = 0;
-
-		mesh7.position.x = x2;
-		mesh7.position.y = x2;
-
-		mesh8.position.x = 0;
-		mesh8.position.y = x2;
-
-		mesh9.position.x = x1;
-		mesh9.position.y = x2;
 
 		// Render finally
 		renderer.render( scene, camera );
@@ -105,16 +61,7 @@ function animate()
 		requestAnimationFrame( animate );
 
 		// make some changes to the object so that it appears in motion
-		rotate(mesh1);
-		rotate(mesh2);
-		rotate(mesh3);
-		rotate(mesh4);
-		rotate(mesh5);
-		rotate(mesh6);
-		rotate(mesh7);
-		rotate(mesh8);
-		rotate(mesh9);
-
+		//rotate(mesh1);
 		renderer.render( scene, camera );
 }
 
