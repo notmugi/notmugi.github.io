4a318c9c1524a013566f03bee72e9d86775c7d5d
Shin <sekolby20@gmail.com>
2020-02-24 23:51:43 -0500

    ddd

--- js/text.js
+++ js/text.js
@@ -5,19 +5,19 @@ function main() {
 
     // renderer
     const renderer = new THREE.WebGLRenderer( { antialias: false,  alpha: true  }); renderer.setClearColor( 0x000000, 0 );
-    renderer.setSize(800, 800);
-    document.body.appendChild(renderer.domElement);
+    renderer.setSize(360, 140);
+    document.getElementById('main').appendChild( renderer.domElement );
 
     // camera
-    const camera = new THREE.PerspectiveCamera(60, 1080 / 1080, 1, 10000);
-    camera.position.set(5, 0, 0); // settings in `sceneList` "Monster"
+    const camera = new THREE.PerspectiveCamera(40, 3080 / 1080, 0.005, 10000);
+    camera.position.set(1.9, 0, 0); // settings in `sceneList` "Monster"
     camera.up.set(0, 1, 0);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 		//camera.zoom(1);
     // scene and lights
     const scene = new THREE.Scene();
     scene.add(new THREE.AmbientLight(0xcccccc));
- 
+
     // load gltf model and texture
     const objs = [];
     const loader = new THREE.GLTFLoader();
@@ -26,12 +26,13 @@ function main() {
 	    sean.traverse((node) => {
 	      if (!node.isMesh) return;
 	      node.material.wireframe = true;
+
 	    });
 
         // settings in `sceneList` "Monster"
-        gltf.scene.scale.set(0.7, 0.7, 0.7);
-        gltf.scene.rotation.set(0, 0, 0);
-        gltf.scene.position.set(2, 1, 0);
+        gltf.scene.scale.set(1, 1, 1);
+        gltf.scene.rotation.set(0, 1.57, 0);
+        gltf.scene.position.set(0, 0, 0);
 
         scene.add(sean);
         objs.push({gltf});
@@ -40,6 +41,9 @@ function main() {
     // animation rendering
     const clock = new THREE.Clock();
     (function animate() {
+      scene.rotation.x += 0.000;
+      scene.rotation.y += 0.005;
+      scene.rotation.z += 0.000;
         // animation with THREE.AnimationMixer.update(timedelta)
         renderer.render(scene, camera);
         requestAnimationFrame(animate);
