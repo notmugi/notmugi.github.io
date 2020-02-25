cfc3a20e6ce42d961cc0294bd23e34b0a7d18e25
Shin <sekolby20@gmail.com>
2020-02-25 11:10:28 -0500

    add navlinks

--- js/text.js
+++ js/text.js
@@ -1,53 +1,46 @@
-"use strict";
-
-// simplified on three.js/examples/webgl_loader_GLTF.html
 function main() {
 
-    // renderer
+// Initialize all important stuff
     const renderer = new THREE.WebGLRenderer( { antialias: false,  alpha: true  }); renderer.setClearColor( 0x000000, 0 );
-    renderer.setSize(360, 140);
-    document.getElementById('main').appendChild( renderer.domElement );
-
-    // camera
+    const loader = new THREE.GLTFLoader();
     const camera = new THREE.PerspectiveCamera(15, 3080 / 1080, 0.005, 10000);
-    camera.position.set(4, 0, 0); // settings in `sceneList` "Monster"
+    const scene = new THREE.Scene();
+
+// set up camera, renderer, scene, and append it all to #main
+    renderer.setSize(360, 140);
+    camera.position.set(4, 0, 0);
     camera.up.set(0, 1, 0);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
-		//camera.zoom(1);
-    // scene and lights
-    const scene = new THREE.Scene();
     scene.add(new THREE.AmbientLight(0xFFFFFFF));
+    document.getElementById('logo').appendChild( renderer.domElement );
 
-    // load gltf model and texture
-    const objs = [];
-    const loader = new THREE.GLTFLoader();
+// use loader to load the gltf & traverse it to set the material
     loader.load("js/sean.gltf", gltf => {
-			var sean = gltf.scene;
-	    sean.traverse((node) => {
-	      if (!node.isMesh) return;
-	      node.material.wireframe = true;
+		var sean = gltf.scene;
+	  sean.traverse((node) => {
+	     if (!node.isMesh) return;
+	       node.material.wireframe = true;
+       });
 
-	    });
+//position scene and place model in center
+    gltf.scene.scale.set(1, 1, 1);
+    gltf.scene.rotation.set(0, 1.57, 0);
+    gltf.scene.position.set(0, 0, 0);
+    scene.add(sean);
+  });
 
-        // settings in `sceneList` "Monster"
-        gltf.scene.scale.set(1, 1, 1);
-        gltf.scene.rotation.set(0, 1.57, 0);
-        gltf.scene.position.set(0, 0, 0);
+// Start animating,render each frame & camera
+  (function animate() {
+    scene.rotation.x += 0.000;
+    scene.rotation.y -= 0.005;
+    scene.rotation.z += 0.000;
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+  })();
 
-        scene.add(sean);
-        objs.push({gltf});
-    });
 
-    // animation rendering
-    const clock = new THREE.Clock();
-    (function animate() {
-      scene.rotation.x += 0.000;
-      scene.rotation.y -= 0.005;
-      scene.rotation.z += 0.000;
-        // animation with THREE.AnimationMixer.update(timedelta)
-        renderer.render(scene, camera);
-        requestAnimationFrame(animate);
-    })();
-    return objs;
+//end of main
 }
-const objs = main();
+
+// this doesnt run main, i swear
+main();
