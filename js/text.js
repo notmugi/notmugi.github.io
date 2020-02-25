bb5f776a910b6e4a9208ef1204ee04efe23c1940
Sean Geromini <sean@ww203196.wwnet.wne.edu>
2020-02-24 19:28:18 -0500

    updated

--- js/text.js
+++ js/text.js
@@ -30,7 +30,7 @@ function main() {
 
         // settings in `sceneList` "Monster"
         gltf.scene.scale.set(0.4, 0.4, 0.4);
-        gltf.scene.rotation.copy(90, 0));
+        gltf.scene.rotation.copy(90, 0);
         gltf.scene.position.set(2, 1, 0);
 
         scene.add(sean);
