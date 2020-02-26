21bff780ced6c7399018aa06e5628559f6be060a
Sean Geromini <sean@ww140229.wwnet.wne.edu>
2020-02-26 09:07:25 -0500

    commit i guess?

---
+++ README.md
@@ -0,0 +1,15 @@
+# Website Readme
+
+This website is going to be my landing page for my github projects. </br>The spinny animation was done in javascript and three.js
+</br></br></br>
+regular link:   <b>https://notmugi.github.io</b>
+</br>
+shortened link: <b>https://femto.pw/x948</b>
+</br></br></br>
+<i>TODO:</i>
+- > ~~Randomize spin direction and speed. up/down & left + speeds~~
+- > make some other colors & theme changer
+- > add links
+- > add transitions
+- > ~~add font~~
+- > ~~shorten link somehow~~
--- js/text.js
+++ js/text.js
@@ -1,23 +1,33 @@
-"use strict";
 
-// simplified on three.js/examples/webgl_loader_GLTF.html
+//Random y and z spin value between 0.0005 and 0.02
+const randSpeedY = Math.random() * (0.02 - 0.0005) + 0.0005;
+const randSpeedZ = Math.random() * (0.02 - 0.0005) + 0.0005;
+
+//Randomize the start type to either 1(y spin) or 0(z spin)
+const randStartType = Math.round(Math.random());
+
+//SpEcIaL SpIn TyPe UwU 1 iN 10 ChAnCe
+const specialSpin = Math.round(Math.random() * (10 - 0) + 0);
+
+
 function main() {
 
-    // renderer
-    const renderer = new THREE.WebGLRenderer( { antialias: false,  alpha: true  }); renderer.setClearColor( 0x000000, 0 );
-    renderer.setSize(800, 800);
-    document.body.appendChild(renderer.domElement);
+// Initialize all important stuff
+    const renderer = new THREE.WebGLRenderer( { antialias: false,  alpha: true  });
+    const loader = new THREE.GLTFLoader();
+    const camera = new THREE.PerspectiveCamera(13, 3080 / 1080, 0.005, 10000);
+    const scene = new THREE.Scene();
 
-    // camera
-    const camera = new THREE.PerspectiveCamera(60, 1080 / 1080, 1, 10000);
-    camera.position.set(5, 0, 0); // settings in `sceneList` "Monster"
+// set up camera, renderer, scene, and append it all to #main
+    renderer.setSize(360, 140);
+    renderer.setClearColor( 0x000000, 0 );
+    camera.position.set(4, 0, 0);
     camera.up.set(0, 1, 0);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
-		//camera.zoom(1);
-    // scene and lights
-    const scene = new THREE.Scene();
-    scene.add(new THREE.AmbientLight(0xcccccc));
+    scene.add(new THREE.AmbientLight(0xFFFFFFF));
+    document.getElementById('logo').appendChild( renderer.domElement );
 
+<<<<<<< HEAD
     // load gltf model and texture
     const objs = [];
     const loader = new THREE.GLTFLoader();
@@ -45,5 +55,46 @@ function main() {
         requestAnimationFrame(animate);
     })();
     return objs;
+=======
+// use loader to load the gltf & traverse it to set the material
+    loader.load("js/sean.gltf", gltf => {
+		var sean = gltf.scene;
+	  sean.traverse((node) => {
+	     if (!node.isMesh) return;
+	       node.material.wireframe = true;
+       });
+
+//position scene and place model in center
+    gltf.scene.scale.set(1, 1, 1);
+    gltf.scene.rotation.set(0, 1.57, 0);
+    gltf.scene.position.set(0, 0, 0);
+    scene.add(sean);
+  });
+
+
+  (function animate() {
+
+//if the random start type is 1, do y spin, else do z spin
+    if (randStartType == 1) {
+      scene.rotation.y -= randSpeedY;
+    }else if (randStartType == 0){
+      scene.rotation.z += randSpeedZ;
+    }
+
+//special spin(both spin types!!!!!!!!!!!!!! wow)
+    if (specialSpin == 10){
+      scene.rotation.y -= randSpeedY;
+      scene.rotation.z += randSpeedZ;
+    }
+
+//render the stuff and things
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+  })();
+
+//end of main
+>>>>>>> a1b91a5c738c2422dd64571605305bbaabfe0f66
 }
-const objs = main();
+
+// this doesnt run main, i swear
+main();
