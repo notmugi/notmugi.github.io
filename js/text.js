6337088509cf4557ac6e0a9a3b2766e8fef2904a
Sean <sekolby20@gmail.com>
2026-04-26 10:09:16 -0400

    inverted colors, added golden glow

--- js/text.js
+++ js/text.js
@@ -1,8 +1,8 @@
-//Random y spin value between 0.0005 and 0.02
-const randSpeedY = Math.random() * (0.02 - 0.0005) + 0.0005;
+//Random y spin value between 0.0005 and 0.001
+const randSpeedY = Math.random() * (0.005 - 0.0005) + 0.0005;
 
-//Random z spin value between 0.0005 and 0.02
-const randSpeedZ = Math.random() * (0.02 - 0.0005) + 0.0005;
+//Random z spin value between 0.0005 and 0.001
+const randSpeedZ = Math.random() * (0.003 - 0.0005) + 0.0005;
 
 //Randomize the start type to either 1(y spin) or 0(z spin)
 const randStartType = Math.round(Math.random());
@@ -20,40 +20,61 @@ function main() {
     const scene = new THREE.Scene();
 
 // set up camera, renderer, scene, and append it all to #main
+    renderer.setPixelRatio(0.7);
     renderer.setSize(360, 140);
     camera.position.set(4, 0, 0);
     camera.up.set(0, 1, 0);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
-    scene.add(new THREE.AmbientLight(0xFFFFFFF));
+    // Ambient light disabled
+    // scene.add(new THREE.AmbientLight(0x333333));
+
+    // Group for the model so lights stay stationary in world space
+    const modelGroup = new THREE.Group();
+    scene.add(modelGroup);
+
+    // Golden point lights (stationary in world space, front-facing, elevated)
+    const pointLight1 = new THREE.PointLight(0xFFD700, 3.0, 50);
+    pointLight1.position.set(4, 0, 2);  // camera position, left side, elevated
+    scene.add(pointLight1);
+
+    const pointLight2 = new THREE.PointLight(0xFFA500, 3.0, 50);
+    pointLight2.position.set(4, 0, -2); // camera position, right side, elevated
+    scene.add(pointLight2);
+
     document.getElementById('logo').appendChild( renderer.domElement );
 
 // use loader to load the gltf & traverse it to set the material
-    loader.load("js/sean.gltf", gltf => {
+    loader.load("js/new.gltf", gltf => {
 		var sean = gltf.scene;
 	  sean.traverse((node) => {
 	     if (!node.isMesh) return;
-	       node.material.wireframe = true;
+	       node.material = new THREE.MeshStandardMaterial({
+	         color: 0x111111,
+	         metalness: 0.0,
+	         roughness: 0.2,
+	         wireframe: true
+	       });
        });
 
 //position scene and place model in center
     gltf.scene.scale.set(1, 1, 1);
     gltf.scene.rotation.set(0, 1.57, 0);
     gltf.scene.position.set(0, 0, 0);
-    scene.add(sean);
+    modelGroup.add(sean);
   });
 
 //if the random start type is 1, do y spin, else do z spin
   (function animate() {
     if (randStartType == 1) {
-      scene.rotation.y -= randSpeedY;
+      modelGroup.rotation.y -= randSpeedY;
     }else if (randStartType == 0){
-      scene.rotation.z += randSpeedZ;
+      modelGroup.rotation.z += randSpeedZ;
     }
 
 //special spin(both spin types!!!!!!!!!!!!!! wow)
     if (specialSpin == 10){
-      scene.rotation.y -= randSpeedY;
-      scene.rotation.z += randSpeedZ;
+      modelGroup.rotation.y -= randSpeedY;
+      modelGroup.rotation.z += randSpeedZ;
     }
 
 //render the stuff and things
