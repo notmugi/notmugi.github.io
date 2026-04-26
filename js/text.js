074aabd61aa9255b7204ab224c25821eac7e5c83
Sean <sekolby20@gmail.com>
2026-04-26 10:11:59 -0400

    inverted colors, added golden glow

--- js/text.js
+++ js/text.js
@@ -1,8 +1,8 @@
 //Random y spin value between 0.0005 and 0.001
-const randSpeedY = Math.random() * (0.005 - 0.0005) + 0.0005;
+const randSpeedY = Math.random() * (0.01 - 0.001) + 0.0005;
 
 //Random z spin value between 0.0005 and 0.001
-const randSpeedZ = Math.random() * (0.003 - 0.0005) + 0.0005;
+const randSpeedZ = Math.random() * (0.01 - 0.001) + 0.0005;
 
 //Randomize the start type to either 1(y spin) or 0(z spin)
 const randStartType = Math.round(Math.random());
